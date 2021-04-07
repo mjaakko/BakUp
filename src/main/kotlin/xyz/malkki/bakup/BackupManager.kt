68f2b201f537f82fda71eee399391e01f7002b68
Jaakko Malkki <jaakko.malkki234@gmail.com>
2021-04-07 19:13:10 +0300

    Create BackupManager for creating backup archives

---
+++ src/main/kotlin/xyz/malkki/bakup/BackupManager.kt
@@ -0,0 +1,67 @@
+package xyz.malkki.bakup
+
+import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream
+import org.apache.commons.compress.compressors.gzip.GzipParameters
+import java.io.BufferedInputStream
+import java.io.IOException
+import java.nio.file.Files
+import java.nio.file.Path
+import java.time.LocalDateTime
+import java.time.temporal.ChronoUnit
+import kotlin.streams.toList
+
+class BackupManager(private val directory: Path, private val outputDirectory: Path, private val maxBackups: Int) {
+    companion object {
+        //Add comment to archives to avoid deleting other files that could be in the output directory (do not change this value)
+        private const val ARCHIVE_COMMENT = "bakup"
+    }
+
+    private fun removeOldestArchives() {
+        val backUpArchiveFiles = Files.list(outputDirectory)
+            .sorted(FileLastModifiedComparator.reversed())
+            .filter { path -> isBackupArchive(path) }
+            .toList()
+
+        println("${backUpArchiveFiles.size} backups found")
+        val newest = backUpArchiveFiles.take(maxBackups - 1).toHashSet()
+        backUpArchiveFiles.forEach { path ->
+            //Remove oldest archives
+            if (!newest.contains(path)) {
+                println("Deleting ${path.fileName}")
+                Files.delete(path)
+            }
+        }
+    }
+
+    private fun isBackupArchive(path: Path): Boolean {
+        try {
+            if ("application/gzip" != Files.probeContentType(path)) {
+                return false
+            }
+
+            val inputStream = GzipCompressorInputStream(BufferedInputStream(Files.newInputStream(path)))
+            inputStream.use {
+                val metadata = it.metaData
+                return ARCHIVE_COMMENT == metadata.comment
+            }
+        } catch (ioe: IOException) {
+            //If it's not possible to determine whether the file is a backup, do not remove it just to be safe
+            return false
+        }
+
+    }
+
+    private fun createFileNameForBackup(): String {
+        //FIXME: this will delete the old file if it has the same name
+        return "${directory.fileName}_${LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS)}.tar.gz"
+    }
+
+    fun takeBackup() {
+        removeOldestArchives()
+        val fileName = createFileNameForBackup()
+        println("Creating backup to $fileName")
+        Archiver.compressDirectory(directory, outputDirectory.resolve(fileName), GzipParameters().apply {
+            comment = ARCHIVE_COMMENT
+        })
+    }
+}
---
+++ src/test/kotlin/xyz/malkki/bakup/BackupManagerTest.kt
@@ -0,0 +1,94 @@
+package xyz.malkki.bakup
+
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry
+import org.apache.commons.compress.archivers.tar.TarArchiveOutputStream
+import org.apache.commons.compress.compressors.gzip.GzipCompressorOutputStream
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TemporaryFolder
+import java.io.BufferedOutputStream
+import java.io.File
+import java.io.FileOutputStream
+import java.nio.file.Files
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class BackupManagerTest {
+    @Rule @JvmField
+    val folder = TemporaryFolder()
+
+    private lateinit var outputDirectory: File
+    private lateinit var backupManager: BackupManager
+
+    @Before
+    fun initialize() {
+        val backupDirectory = folder.newFolder("backup")
+        val a = File(backupDirectory, "a.txt")
+        a.writeText("a")
+        val b = File(backupDirectory, "b.txt")
+        b.writeText("b")
+
+        outputDirectory = folder.newFolder("output")
+
+        backupManager = BackupManager(backupDirectory.toPath(), outputDirectory.toPath(), 3)
+    }
+
+    @Test
+    fun `Test that correct amount of backup archives is created`() {
+        backupManager.takeBackup()
+        assertEquals(1, outputDirectory.list().size)
+
+        Thread.sleep(1500)
+
+        backupManager.takeBackup()
+        assertEquals(2, outputDirectory.list().size)
+
+        Thread.sleep(1500)
+
+        backupManager.takeBackup()
+        assertEquals(3, outputDirectory.list().size)
+
+        Thread.sleep(1500)
+
+        //Max 3 backups
+        backupManager.takeBackup()
+        assertEquals(3, outputDirectory.list().size)
+    }
+
+    @Test
+    fun `Test that other files are not deleted from output directory`() {
+        val archiveFile = createArchive(outputDirectory)
+        val testFile = File(outputDirectory, "test.txt")
+        testFile.writeText("test")
+
+        backupManager.takeBackup()
+        Thread.sleep(1500)
+        backupManager.takeBackup()
+        Thread.sleep(1500)
+        backupManager.takeBackup()
+        Thread.sleep(1500)
+        backupManager.takeBackup()
+
+        assertTrue(archiveFile.exists())
+        assertTrue(testFile.exists())
+    }
+
+    /**
+     * Creates an archive file to output directory to test that unrelated archives are not deleted by BackupManager
+     */
+    private fun createArchive(directory: File): File {
+        val archiveContent = folder.newFile("test.txt")
+        archiveContent.writeText("test")
+
+        val archiveFile = File(directory, "test.tar.gz")
+        val outputStream = TarArchiveOutputStream(GzipCompressorOutputStream(BufferedOutputStream(FileOutputStream(archiveFile))))
+        outputStream.use {
+            val entry = TarArchiveEntry(archiveContent, "test.txt")
+            it.putArchiveEntry(entry)
+            Files.copy(archiveContent.toPath(), it)
+            it.closeArchiveEntry()
+        }
+        return archiveFile
+    }
+}
