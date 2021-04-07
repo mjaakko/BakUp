30618225c90150e8cb635059d143a59401c0ecbf
Jaakko Malkki <jaakko.malkki234@gmail.com>
2021-04-07 21:32:26 +0300

    Create main function for running the application

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,40 @@
+
+import org.apache.commons.cli.DefaultParser
+import org.apache.commons.cli.HelpFormatter
+import org.apache.commons.cli.Options
+import xyz.malkki.bakup.BackupManager
+import java.nio.file.Files
+import java.nio.file.Paths
+
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val cliOptions = Options().apply {
+        addOption("d", "directory", true, "The directory which will be backed up")
+        addOption("o", "outputDirectory", true, "The directory where the backup archives are stored")
+        addOption("n", "maxBackups", true, "The maximum number of backups that will be stored")
+        addOption("h", "help", false, "Print this message")
+    }
+    val optionsParser = DefaultParser()
+    val cli = optionsParser.parse(cliOptions, args)
+
+    //Show help message
+    val showHelp = cli.options.isEmpty() || cli.hasOption("h")
+    if (showHelp) {
+        val helpFormatter = HelpFormatter()
+        helpFormatter.printHelp("bakup [OPTIONS]", cliOptions)
+        return
+    }
+
+    val directory = Paths.get(cli.getOptionValue("d"))
+    val outputDirectory = Paths.get(cli.getOptionValue("o"))
+    val maxBackups = cli.getOptionValue("n").toInt()
+
+    try {
+        Files.createDirectories(outputDirectory)
+    } catch (e: Exception) {
+        println("Failed to create output directory for backup archives: ${e.message}")
+        return
+    }
+
+    val backupManager = BackupManager(directory, outputDirectory, maxBackups)
+    backupManager.takeBackup()
 }
